74b33f206cba6815782690e8b9626fe9046b373c
DmytroRev <149614107+DmytroRev@users.noreply.github.com>
2024-07-17 12:32:49 +0300

    Update server.js

--- src/server.js
+++ src/server.js
@@ -20,6 +20,7 @@ export const setupServer = () => {
         try {
             const contacts = await getAllContacts();
             res.status(200).json({
+                status: 200,
                 message: "Successfully found contacts!",
                 data: contacts
             });
