6d61a101c882595c737ff31b0977b841fc6cf355
DmytroRev <149614107+DmytroRev@users.noreply.github.com>
2024-08-02 16:25:46 +0300

    create router login

--- src/routers/auth.js
+++ src/routers/auth.js
@@ -1,8 +1,11 @@
 import { Router } from 'express';
 import { validateBody } from '../utils/validateBody.js';
 import { ctrlWrapper } from '../utils/ctrlWrapper.js';
-import { registerUserSchema } from '../validation/auth.js';
-import { registerUserController } from '../controllers/auth.js';
+import { loginUserSchema, registerUserSchema } from '../validation/auth.js';
+import {
+  registerUserController,
+  loginUserController,
+} from '../controllers/auth.js';
 const router = Router();
 
 router.post(
@@ -10,5 +13,9 @@ router.post(
   validateBody(registerUserSchema),
   ctrlWrapper(registerUserController),
 );
-
+router.post(
+  '/login',
+  validateBody(loginUserSchema),
+  ctrlWrapper(loginUserController),
+);
 export default router;
--- src/services/auth.js
+++ src/services/auth.js
@@ -1,6 +1,9 @@
 import createHttpError from 'http-errors';
 import { UsersCollection } from '../db/models/user.js';
 import bcrypt from 'bcrypt';
+import { SessionCollection } from '../db/models/session.js';
+import crypto from 'node:crypto';
+import { FIFTEEN_MINUTES, ONE_DAY } from '../constants/index.js';
 
 export const registerUser = async (userReg) => {
   const user = await UsersCollection.findOne({ email: userReg.email });
@@ -10,5 +13,27 @@ export const registerUser = async (userReg) => {
   return await UsersCollection.create({
     ...userReg,
     password: encryptedPassword,
+  });
+};
+
+export const loginUser = async (userLog) => {
+  const user = await UsersCollection.findOne({ email: userLog.email });
+
+  if (user === null) throw createHttpError(404, 'User not found');
+
+  const isEqual = await bcrypt.compare(userLog.password, user.password);
+
+  if (!isEqual) throw createHttpError(401, 'Unauthorized');
+  await SessionCollection.deleteOne({ userId: user._id });
+
+  const accessToken = crypto.randomBytes(30).toString('base64');
+  const refreshToken = crypto.randomBytes(30).toString('base64');
+
+  return await SessionCollection.create({
+    userId: user._id,
+    accessToken,
+    refreshToken,
+    accessTokenValidUntil: new Date(Date.now() + FIFTEEN_MINUTES),
+    refreshTokenValidUntil: new Date(Date.now() + ONE_DAY),
   });
 };
