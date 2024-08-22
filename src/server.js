14233a3a3424b4732f28b2c395c0c6963f492b66
DmytroRev <149614107+DmytroRev@users.noreply.github.com>
2024-08-22 16:30:26 +0300

    hw07

--- src/server.js
+++ src/server.js
@@ -7,6 +7,7 @@ import contactsRouter from './routers/contacts.js';
 import authRouter from './routers/auth.js';
 import cookieParser from 'cookie-parser';
 import { UPLOAD_DIR } from './constants/index.js';
+import { swaggerDocs } from './middlewares/swaggerDocs.js';
 
 const PORT = process.env.PORT || 3000;
 
@@ -32,6 +33,7 @@ export const setupServer = () => {
   app.use('/contacts', contactsRouter);
   app.use('/auth', authRouter);
   app.use('/uploads/photos', express.static(UPLOAD_DIR));
+  app.use('/api-docs', swaggerDocs());
   app.use('*', notFoundHandler);
 
   app.use(errorHandler);
